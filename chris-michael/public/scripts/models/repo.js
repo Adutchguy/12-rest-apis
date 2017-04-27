1c6c956cfdbda17ba2fc61857d6cd233d311bd42
Adutchguy <mdm_8@hotmail.com>
2017-04-27 14:15:14 -0700

    biaaa

--- chris-michael/public/scripts/models/repo.js
+++ chris-michael/public/scripts/models/repo.js
@@ -10,10 +10,6 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-    //       Remember that the callback function we'll want to call relies on repos.all
-    //       being an array with a bunch of repo objects in it, so you'll need to
-    //       populate it with the response from Github before you call the callback.
     $.get(`https://api.github.com/users/Adutchguy/repos?access_token=${gitToken}`)
     .then(
       results => {
